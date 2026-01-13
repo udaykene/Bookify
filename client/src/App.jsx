30a2ae3a1af364c132771814287a5b285d8e6f5f
priyanshshigawan-star <priyanshshigawan@gmail.com>
2026-01-13 19:40:57 +0530

    added new file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home.jsx';
 import Footer from './components/Footer.jsx';
 import AllRooms from './pages/AllRooms.jsx';
+import RoomDetails from './pages/RoomDetails.jsx';
 
 const App = () => {
 
@@ -15,7 +16,9 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path="/" element={<Home />} />
-           <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+
         </Routes>
 
       </div>

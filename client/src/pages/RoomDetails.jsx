6d8b67c0f3e040d6efb778fb5f97e256d97dc4cc
priyanshshigawan-star <priyanshshigawan@gmail.com>
2026-01-16 14:08:40 +0530

    Add Room Details

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { assets, facilityIcons, roomsDummyData } from '../assets/assets';
+import { assets, facilityIcons, roomCommonData, roomsDummyData } from '../assets/assets';
 import StarRating from '../components/StarRating';
 
 const RoomDetails = () => {
@@ -59,6 +59,84 @@ const RoomDetails = () => {
             </div>
             {/* Room Price */}
             <p className='text-2xl font-medium'>${room.pricePerNight}/night</p>
+
+
+        </div>
+        {/* CheckIn CheckOut Form */}
+        <form className='flex flex-col md:flex-row items:start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
+            <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+                <div className='flex flex-col'>
+                    <label htmlFor='checkInDate' className='font-medium'> Check-In</label>
+                    <input type='date' id='checkInDate' placeholder='Check-In' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                </div>
+
+                <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+
+                <div className='flex flex-col'>
+                    <label htmlFor='checkOutDate' className='font-medium'> Check-Out</label>
+                    <input type='date' id='checkOutDate' placeholder='Check-Out' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                </div>
+
+                <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+
+                <div className='flex flex-col'>
+                    <label htmlFor='guests' className='font-medium'> Guests</label>
+                    <input type='number' id='guests' placeholder='0' className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                </div>
+
+
+
+            </div>
+            <button type="submit"
+            className="bg-blue-600 hover:bg-blue-700 active:scale-95 transition-all text-white rounded-md 
+             max-md:w-full max-md:mt-6 md:px-24 py-3 md:py-4 text-base cursor-pointer"
+>             Check Availability
+            </button>
+
+
+        </form>
+
+        {/* Common Specification */}
+        <div className='mt-25 space-y-4'>
+            {roomCommonData.map((spec, index)=>(
+                <div key={index} className='flex items-start gap-2'>
+                    <img src={spec.icon} alt={`${spec.title}-icon`} className='w-6.5'/>
+                    <div>
+                        <p className='text-base'>{spec.title}</p>
+                        <p className='text-gray-500'>{spec.description}</p>
+                    </div>
+
+                </div>
+
+            ))}
+        </div>
+
+        <div className='max-w-3xl border-y border-gray-300 my-15 py-10 text-gray-500'>
+            <p>Guests will be allocated on the ground floor according to Availability.
+                You get a comfortable Two bedroom apartment has a true city feeling. the
+                price quoted is for two guest, at the guest slot please mark the number of
+                guest to get the exact price for groups. The Guests will be allocated
+                ground floor according to Availability. You get the comfortable two bedroom
+                apartment that has a true city feeling.
+            </p>
+        </div>
+        {/* Hosted by */}
+
+        <div className='flex flex-col items-start gap-4'>
+            <div className='flex gap-4'>
+                <img src={room.hotel.owner.image} alt='Host' className='h-14 w-14 md:h-18 md:w-18 rounded-full'/>
+                <div>
+                    <p className='text-lg md:text-xl'>Hosted by {room.hotel.name}</p>
+                    <div className='flex item-center mt-1'>
+                        <StarRating />
+                        <p className='ml-2'> 200+ reviews</p>
+                    </div>
+                </div>
+
+            </div>
+        <button className="px-6 py-2.5 mt-4 rounded text-white bg-blue-600 hover:bg-blue-700 transition-all cursor-pointer">
+         Contact Now
+        </button>
 
         </div>
 
